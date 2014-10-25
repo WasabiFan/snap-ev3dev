4ea35039771c4e89a2386ae6132cfa8b710583a7
WasabiFan <wasabifan@outlook.com>
2014-10-25 07:25:40 +0000

    Added basic ev3dev function infrastructure w/ test action

--- snap-server.js
+++ snap-server.js
@@ -4,14 +4,19 @@ var http = require('http');
 var fileServer = new static.Server("./snap");
 
 http.createServer(function (req, res) {
-	var reqPath = require('url').parse(req.url);
+	var reqPath = require('url').parse(req.url, true);
 	if(reqPath.pathname == '/') {
 		console.log('Serving default Snap! page');
 		fileServer.serveFile('/snap.html', 200, {}, req, res);
 	}
 	else if (reqPath.pathname.indexOf('/ev3dev') == 0) {
-		console.log('Request for ev3 function');
-		//TODO: implement
+		var pathParts = reqPath.pathname.split('/');
+		console.log('Request for ev3 function: ' + pathParts);
+		
+		if(pathParts[pathParts.length - 1] == "sensor") {
+			console.log('Request for sensor "' + reqPath.query['portName'] +  '" and value index "' + reqPath.query['index'] +  '"');
+			res.end("10937");
+		}
 	}
 	else {
 		console.log('Serving "' + reqPath.pathname  + '"');
